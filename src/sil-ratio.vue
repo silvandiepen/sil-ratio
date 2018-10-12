5ce21db47a6f02400a1ff80a11418142bae2a3ee
Sil van Diepen <me@silvandiepen.com>
2018-10-12 17:19:00 +0200

    it should work now but has double code

--- src/sil-ratio.vue
+++ src/sil-ratio.vue
@@ -1,34 +1,34 @@
 <script>
-const setStyle = function(el) {
-	let check = {
-		small: 0,
-		medium: 750,
-		large: 1024,
-		xlarge: 1440,
-		xxlarge: 1920
-	};
-	let ratio = binding.value;
-
-	if (!process.server) {
-		Object.keys(check).forEach(function(item) {
-			if (window.innerWidth > check[item]) {
-				if (el.getAttribute('ratio-' + item)) {
-					ratio = el.getAttribute('ratio-' + item);
-				}
-			}
-		});
-	}
-
-	ratio = ratio.split(':');
-	let elSize = el.getBoundingClientRect();
-	el.style.height = (elSize.width / ratio[0]) * ratio[1] + 'px';
-	el.style.opacity = 1;
-	if (el.classList.contains('ratio-hide')) {
-		el.classList.remove('ratio-hide');
-	}
-};
 export default {
 	bind: function(el, binding) {
+		let setStyle = function(el) {
+			let check = {
+				small: 0,
+				medium: 750,
+				large: 1024,
+				xlarge: 1440,
+				xxlarge: 1920
+			};
+			let ratio = binding.value;
+
+			if (!process.server) {
+				Object.keys(check).forEach(function(item) {
+					if (window.innerWidth > check[item]) {
+						if (el.getAttribute('ratio-' + item)) {
+							ratio = el.getAttribute('ratio-' + item);
+						}
+					}
+				});
+			}
+
+			ratio = ratio.split(':');
+			let elSize = el.getBoundingClientRect();
+			el.style.height = (elSize.width / ratio[0]) * ratio[1] + 'px';
+			el.style.opacity = 1;
+			if (el.classList.contains('ratio-hide')) {
+				el.classList.remove('ratio-hide');
+			}
+		};
 		setStyle(el);
 		if (!process.server) {
 			window.addEventListener('resize', function() {
@@ -37,6 +37,34 @@ export default {
 		}
 	},
 	update: function(el, binding, vnode) {
+		let setStyle = function(el) {
+			let check = {
+				small: 0,
+				medium: 750,
+				large: 1024,
+				xlarge: 1440,
+				xxlarge: 1920
+			};
+			let ratio = binding.value;
+
+			if (!process.server) {
+				Object.keys(check).forEach(function(item) {
+					if (window.innerWidth > check[item]) {
+						if (el.getAttribute('ratio-' + item)) {
+							ratio = el.getAttribute('ratio-' + item);
+						}
+					}
+				});
+			}
+
+			ratio = ratio.split(':');
+			let elSize = el.getBoundingClientRect();
+			el.style.height = (elSize.width / ratio[0]) * ratio[1] + 'px';
+			el.style.opacity = 1;
+			if (el.classList.contains('ratio-hide')) {
+				el.classList.remove('ratio-hide');
+			}
+		};
 		setStyle(el);
 	},
 	inserted: function(el, binding) {
