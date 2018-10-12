a0ae0efcfbf1a6a1e936de4d87be802c6e05f3d8
Sil van Diepen <me@silvandiepen.com>
2018-10-12 17:11:47 +0200

    another try to win the world

--- src/sil-ratio.vue
+++ src/sil-ratio.vue
@@ -1,45 +1,43 @@
 <script>
+const setStyle = function(el) {
+	let check = {
+		small: 0,
+		medium: 750,
+		large: 1024,
+		xlarge: 1440,
+		xxlarge: 1920
+	};
+	let ratio = binding.value;
+
+	if (!process.server) {
+		Object.keys(check).forEach(function(item) {
+			if (window.innerWidth > check[item]) {
+				if (el.getAttribute('ratio-' + item)) {
+					ratio = el.getAttribute('ratio-' + item);
+				}
+			}
+		});
+	}
+
+	ratio = ratio.split(':');
+	let elSize = el.getBoundingClientRect();
+	el.style.height = (elSize.width / ratio[0]) * ratio[1] + 'px';
+	el.style.opacity = 1;
+	if (el.classList.contains('ratio-hide')) {
+		el.classList.remove('ratio-hide');
+	}
+};
 export default {
 	bind: function(el, binding) {
-		this.setStyle(el);
+		setStyle(el);
 		if (!process.server) {
 			window.addEventListener('resize', function() {
-				this.setStyle(el);
+				setStyle(el);
 			});
 		}
 	},
-	methods: {
-		setStyle(el) {
-			let check = {
-				small: 0,
-				medium: 750,
-				large: 1024,
-				xlarge: 1440,
-				xxlarge: 1920
-			};
-			let ratio = binding.value;
-
-			if (!process.server) {
-				Object.keys(check).forEach(function(item) {
-					if (window.innerWidth > check[item]) {
-						if (el.getAttribute('ratio-' + item)) {
-							ratio = el.getAttribute('ratio-' + item);
-						}
-					}
-				});
-			}
-
-			ratio = ratio.split(':');
-			let elSize = el.getBoundingClientRect();
-			el.style.height = (elSize.width / ratio[0]) * ratio[1] + 'px';
-			el.style.opacity = 1;
-			if (el.classList.contains('ratio-hide')) {
-				el.classList.remove('ratio-hide');
-			}
-		}
-	},
 	update: function(el, binding, vnode) {
-		this.setStyle(el);
+		setStyle(el);
 	},
 	inserted: function(el, binding) {
 		//added this function twice cause I couldn't call setStyle function in a method, maybe fix later
